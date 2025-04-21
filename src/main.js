8721a478b3f067362928d8cca6ed0fefdfb247a5
rafim699956 <rafim699956@gmail.com>
2025-04-21 12:56:09 +0600

    home page style end

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import "./style.css";
+const harberger_btn = document.getElementById("hamberger_btn");
+const header_menu = document.getElementById("header_menu");
+harberger_btn.addEventListener("click", () => {
+	header_menu.classList.toggle("hidden");
+});
