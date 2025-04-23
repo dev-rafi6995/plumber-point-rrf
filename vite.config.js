af2ab9379fdcbfd8d0563bfe4fcafd28ce3f0bbf
rafim699956 <rafim699956@gmail.com>
2025-04-23 12:28:56 +0600

    add build in css file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, "index.html"),
+				about: path.resolve(__dirname, "about-us.html"),
+				service: path.resolve(__dirname, "service.html"),
+				project: path.resolve(__dirname, "our-project.html"),
+				blog: path.resolve(__dirname, "our-blog.html"),
+				contact: path.resolve(__dirname, "contact.html"),
+			},
+		},
+	},
 	plugins: [tailwindcss()],
 });
